b04115a1e8dc593c211cc486c4bd614141820b3f
Kamil Radomski <kamil.radomski@relativity.com>
2023-01-10 22:35:13 -0600

    Added Settings state

---
+++ SatisfactoryProductionator/Shared/States/SettingsState.cs
@@ -0,0 +1,18 @@
+﻿using SatisfactoryProductionator.Shared.States.Models;
+
+namespace SatisfactoryProductionator.Shared.States
+{
+	public class SettingsState
+	{
+		public AppSettings Value { get; set; }
+		public event Action OnStateChange;
+
+		public void SetValue(AppSettings value)
+		{
+			this.Value = value;
+			NotifyStateChanged();
+		}
+
+		private void NotifyStateChanged() => OnStateChange?.Invoke();
+	}
+}
