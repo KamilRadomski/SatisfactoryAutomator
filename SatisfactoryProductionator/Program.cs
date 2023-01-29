28c20b537ec3b8ca33aa8a2d34bb6a57245cca7e
Kamil Radomski <radkamil@gmail.com>
2023-01-29 13:26:48 -0600

    Moved icons under images

--- SatisfactoryProductionator/Program.cs
+++ SatisfactoryProductionator/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Hosting;
 using SatisfactoryProductionator;
 using SatisfactoryProductionator.Shared.States;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
