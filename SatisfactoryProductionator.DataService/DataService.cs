c04ce493cf957426a553bd9c173b75ade4debd83
Kamil Radomski <radkamil@gmail.com>
2023-09-14 08:41:50 -0500

    Readded buildings and infra with new slim jsons

--- SatisfactoryProductionator.DataService/DataService.cs
+++ SatisfactoryProductionator.DataService/DataService.cs
@@ -35,6 +35,8 @@ namespace SatisfactoryProductionator.DataService
 
             entries = entries.Concat(await ParseItems()).ToList();
             entries = entries.Concat(await ParseEquipment()).ToList();
+            entries = entries.Concat(await ParseBuildings()).ToList();
+            entries = entries.Concat(await ParseInfrastructure()).ToList();
 
             codex.CodexItems = entries;
 
@@ -55,6 +57,24 @@ namespace SatisfactoryProductionator.DataService
             var content = await _httpClient.GetStringAsync(Constants.EQUIPMENT_FILEPATH);
 
             var items = JsonSerializer.Deserialize<List<Equipment>>(content)!.ToList<CodexItem>();
+
+            return items;
+        }
+
+        private async Task<List<CodexItem>> ParseBuildings()
+        {
+            var content = await _httpClient.GetStringAsync(Constants.BUILDING_FILEPATH);
+
+            var items = JsonSerializer.Deserialize<List<Building>>(content)!.ToList<CodexItem>();
+
+            return items;
+        }
+
+        private async Task<List<CodexItem>> ParseInfrastructure()
+        {
+            var content = await _httpClient.GetStringAsync(Constants.INFRA_FILEPATH);
+
+            var items = JsonSerializer.Deserialize<List<Infrastructure>>(content)!.ToList<CodexItem>();
 
             return items;
         }
