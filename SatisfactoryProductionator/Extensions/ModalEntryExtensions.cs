88af65836b4a7dab5e341ea856c8f7da89afde77
Kamil Radomski <radkamil@gmail.com>
2023-09-26 21:51:04 -0500

    Added ModalEntry extensions

---
+++ SatisfactoryProductionator/Extensions/ModalEntryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using SatisfactoryProductionator.DataModels.Enums;
+using SatisfactoryProductionator.DataModels.Models;
+
+namespace SatisfactoryProductionator.Extensions
+{
+    public static class ModalEntryExtensions
+    {
+        public static PageType GetPageType(this ModalEntry modalEntry)
+        {
+            var index = modalEntry.Index;
+
+            return modalEntry.CodexItem.Pages[index].PageType;
+        }
+
+        public static string GetPageHeader(this ModalEntry modalEntry)
+        {
+            return modalEntry.GetPageType().ToString();
+        }
+    }
+}
