6a62aeb35234c2a6b86c8433b53bf9c841b4ba57
Kamil Radomski <radkamil@gmail.com>
2023-10-14 06:51:11 -0500

    Added Pattern customizer items

---
+++ SatisfactoryProductionator.DataModels/Enums/MaterialType.cs
@@ -0,0 +1,15 @@
+﻿namespace SatisfactoryProductionator.DataModels.Enums
+{
+	public enum MaterialType
+	{
+		Default = 0,
+		Ficsit = 1,
+		Grip = 2,
+		Concrete = 3,
+		CoatedConcrete = 4,
+		Asphalt = 5,
+		Steel = 6,
+		Tar = 7,
+		Glass = 8,
+	}
+}
--- SatisfactoryProductionator.DataModels/Models/Codex/Infrastructure.cs
+++ SatisfactoryProductionator.DataModels/Models/Codex/Infrastructure.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SatisfactoryProductionator.DataModels.Enums;
 
 namespace SatisfactoryProductionator.DataModels.Models.Codex
 {
     public class Infrastructure : CodexEntry
     {
-
+        public MaterialType MaterialType { get; set; }
     }
 }
