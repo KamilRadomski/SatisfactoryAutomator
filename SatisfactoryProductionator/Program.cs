3b937a9fbb864e803f69c80a47a7324d5aea3985
Kamil Radomski <radkamil@gmail.com>
2023-12-21 08:36:36 -0600

    Updated MenuState for reusing side menu on all pages

--- SatisfactoryProductionator/Program.cs
+++ SatisfactoryProductionator/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(build
 
 builder.Services.AddSingleton<SettingsState>();
 builder.Services.AddSingleton<CodexState>();
-builder.Services.AddSingleton<CodexMenuState>();
+builder.Services.AddSingleton<MenuState>();
 builder.Services.AddSingleton<CodexModalState>();
 
 await builder.Build().RunAsync();
